a5932adbbe660b8868bc50aff5c23389c31992a7
phoebe velma awuor <awuorphoebi@gmail.com>
2024-12-04 07:23:21 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+function superbowlWin(record) {
+    const win = record.find(entry=>entry.results ==="W")
+    return win ? win.year: "2015";
+   
+
+}
+const result=superbowlWin(record);
+console.log(result);
+
+
